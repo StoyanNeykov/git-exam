9b791a789e5ee29ca072c3331e5a4608da2d85e4
StoyanNeykov <stoian_neikov@abv.bg>
2026-06-22 16:11:08 +0300

    Testing pre-commit and pre-push

--- test_age_checker.py
+++ test_age_checker.py
@@ -5,11 +5,7 @@ from age_checker import is_adult
 @pytest.mark.parametrize(
     "age, expected",
     [
-        (17, False),
-        (18, True),
-        (19, True),
-        (0, False),
-        (100, True),
+        (17, False)
     ],
 )
 def test_is_adult(age, expected):
