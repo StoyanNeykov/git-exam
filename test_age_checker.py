104715928a2edcfb4d3f9687fc9009a4546cf7b9
StoyanNeykov <stoian_neikov@abv.bg>
2026-06-22 16:12:07 +0300

    Testing pre-push

--- test_age_checker.py
+++ test_age_checker.py
@@ -4,9 +4,7 @@ from age_checker import is_adult
 
 @pytest.mark.parametrize(
     "age, expected",
-    [
-        (17, False)
-    ],
+    [(17, False)],
 )
 def test_is_adult(age, expected):
     assert is_adult(age) == expected
